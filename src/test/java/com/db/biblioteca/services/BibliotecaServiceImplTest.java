8ed003cef8aaa19afa7bd12cc596032d4bbf309f
pedro felipe <pedrofelipinho@gmail.com>
2024-05-10 11:40:10 -0300

    Concluindo os testes da implementação do serviço da biblioteca

--- src/test/java/com/db/biblioteca/services/BibliotecaServiceImplTest.java
+++ src/test/java/com/db/biblioteca/services/BibliotecaServiceImplTest.java
@@ -1,9 +1,18 @@
 package com.db.biblioteca.services;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -74,5 +83,87 @@ public class BibliotecaServiceImplTest {
     @DisplayName("Deve falhar ao buscar biblioteca por id")
     void deveFalharAoBuscarBibliotecaPorId() {
         assertThrows(NoSuchElementException.class, () -> this.bibliotecaServiceImpl.buscarBibliotecaPorId(1L));
+    }
+
+    @Test
+    @DisplayName("Deve remover um livro")
+    void deveRemoverUmLivro() {
+        this.biblioteca.getLivros().add(livro);
+        when(this.bibliotecaRepository.findById(1L)).thenReturn(Optional.of(this.biblioteca));
+        when(this.livroServiceImpl.buscarLivroPorId(1L)).thenReturn(this.livro);
+
+        assertDoesNotThrow(() -> this.bibliotecaServiceImpl.removerLivroDaBiblioteca(1L, 1L));
+
+        verify(this.bibliotecaRepository, times(1)).findById(1L);
+        verify(this.livroServiceImpl, times(1)).buscarLivroPorId(1L);
+        verify(this.bibliotecaRepository, times(1)).save(this.biblioteca);
+    }
+
+    @Test
+    @DisplayName("Deve falhar ao remover um livro")
+    void deveFalharAoRemoverUmLivro() {
+        when(this.bibliotecaRepository.findById(1L)).thenReturn(Optional.of(this.biblioteca));
+        when(this.livroServiceImpl.buscarLivroPorId(1L)).thenReturn(this.livro);
+
+        assertThrows(NoSuchElementException.class, () -> this.bibliotecaServiceImpl.removerLivroDaBiblioteca(1L, 1L));
+
+        verify(this.bibliotecaRepository, times(1)).findById(1L);
+        verify(this.livroServiceImpl, times(1)).buscarLivroPorId(1L);
+        verify(this.bibliotecaRepository, never()).save(any());
+    }
+
+    @Test
+    @DisplayName("Deve criar uma biblioteca")
+    void deveCriarUmaBiblioteca() {
+        Biblioteca biblioteca = BibliotecaFixture.gerarBiblioteca();
+
+        when(this.bibliotecaRepository.save(any())).thenReturn(biblioteca);
+
+        Biblioteca bibliotecaSalva = this.bibliotecaServiceImpl.criarBiblioteca(biblioteca);
+
+        assertNotNull(bibliotecaSalva);
+        assertEquals(biblioteca, bibliotecaSalva);
+
+        verify(this.bibliotecaRepository, times(1)).save(any());
+    }
+
+    @Test
+    @DisplayName("Deve remover uma biblioteca por ID")
+    void deveRemoverUnaBibliotecaPorId() {
+        when(bibliotecaRepository.findById(1L)).thenReturn(Optional.of(biblioteca));
+
+        assertDoesNotThrow(() -> bibliotecaServiceImpl.removerBibliotecaPorId(1L));
+
+        verify(bibliotecaRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("Deve falhar ao remover uma biblioteca por ID ")
+    void deveFalharAoRemoverBibliotecaPorId() {
+        when(bibliotecaRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(NoSuchElementException.class, () -> bibliotecaServiceImpl.removerBibliotecaPorId(1L));
+
+        verify(bibliotecaRepository, times(1)).findById(1L);
+
+        verify(bibliotecaRepository, never()).delete(any());
+    }
+
+    @Test
+    @DisplayName("Deve buscar todas as bibliotecas")
+    void deveBuscarTodasBibliotecas() {
+        List<Biblioteca> bibliotecas = new ArrayList<>();
+        bibliotecas.add(biblioteca);
+
+        when(bibliotecaRepository.findAll()).thenReturn(bibliotecas);
+
+        List<Biblioteca> bibliotecasRetornadas = bibliotecaServiceImpl.buscarTodasBibliotecas();
+
+        assertNotNull(bibliotecasRetornadas);
+
+        assertEquals(1, bibliotecasRetornadas.size());
+        assertEquals(biblioteca.getNome(), bibliotecasRetornadas.get(0).getNome());
+
+        verify(bibliotecaRepository, times(1)).findAll();
     }
 }
